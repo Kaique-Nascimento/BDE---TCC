517f6fb37ea611062753a31ac10286f99b9889a0
Kaique Nascimento <kaiquepalmanascimento@hotmail.com>
2023-08-11 14:33:46 -0300

    Add files via upload

---
+++ tentativa16/categoria.php
@@ -0,0 +1,14 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+include_once "conexao.php";
+
+$sql = "SELECT tb04_nome_genero FROM tb04_categoria";
+
+$comando = $banco->prepare($sql);
+$comando->execute();
+
+$registros = $comando->fetchAll();
+
+echo json_encode($registros);
+
+

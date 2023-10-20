abd29187a8e1908c00bc502df76323f178a48fe5
Kaique Nascimento <kaiquepalmanascimento@hotmail.com>
2023-10-20 18:30:07 -0300

    Add files via upload

---
+++ tentativa49/README.md
@@ -0,0 +1,2 @@
+# BookVerse - Biblioteca Digital para Estudantes
+BookVerse - Biblioteca Digital para Estudantes é um projeto de Trabalho de Conclusão de Curso que atualmente estou colocando meu tempo. 
---
+++ tentativa49/php/autores/foto_cadastro.php
@@ -0,0 +1,44 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+include_once "../conexao.php";
+
+if (isset($_POST["codigo"]) && isset($_FILES["arquivo"])) {
+    $codigo = $_POST["codigo"];
+    $dir = "../../img/autores"; 
+    $foto = $_FILES["arquivo"];
+    
+    // Verificar se a foto já existe no diretório
+    $nomeImagem = pathinfo($foto["name"], PATHINFO_FILENAME);
+    $nomeImagem = $nomeImagem . '.png';
+    $caminhoCompleto = "$dir/$nomeImagem";
+    
+    if (file_exists($caminhoCompleto)) {
+        $mensagem = "Uma foto com o mesmo nome já existe. Escolha outro nome para a imagem.";
+    } else {
+        if ($foto["error"] == 0) {
+            if (move_uploaded_file($foto["tmp_name"], $caminhoCompleto)) {
+                $sql = "UPDATE tb03_autor
+                SET
+                    tb03_foto=?
+                WHERE tb03_id_autor=?";
+
+                try {
+                    $comando = $banco->prepare($sql);
+                    $comando->execute(array($nomeImagem, $codigo));
+                    $mensagem = "Registro inserido com sucesso!";
+                } catch (PDOException $e) {
+                    $mensagem = "Erro ao inserir o registro: " . $e->getMessage();
+                }
+            } else {
+                $mensagem = "Erro ao mover o arquivo para o diretório de destino.";
+            }
+        } else {
+            $mensagem = "Erro no envio do arquivo. Código de erro: " . $foto["error"];
+        }
+    }
+} else {
+    $mensagem = "Dados incompletos.";
+}
+
+echo json_encode(array("mensagem" => $mensagem));
+?>
